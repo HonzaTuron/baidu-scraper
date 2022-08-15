8a34c30f4f385dc54464fe50b9666bae2932def6
HonzaTuron <jan.turon@apify.com>
2022-08-15 15:20:21 +0200

    Fix dataset push data

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { resolveBaiduLink, resolveResultsCount } from "@/utils";
-import { Actor } from "apify";
-import { PlaywrightCrawler } from "@crawlee/playwright";
+import { KeyValueStore, Dataset, PlaywrightCrawler } from "crawlee";
 
 interface Link {
   url: string;
@@ -24,84 +23,77 @@ interface Input {
   pages?: number;
 }
 
-Actor.main(async () => {
-  const dataset = await Actor.openDataset("default");
+const crawler = new PlaywrightCrawler({
+  requestHandler: async ({ page }) => {
+    const resultElements = await page.$$("h3");
+    const results: ResultLink[] = [];
+    for (const titleElement of resultElements) {
+      const title = (await titleElement.textContent()) || "";
+      const linkElement = await titleElement.$("a");
+      const linkUrl = await linkElement?.getAttribute("href");
 
-  const crawler = new PlaywrightCrawler({
-    requestHandler: async ({ page }) => {
-      const resultElements = await page.$$("h3");
-      const results: ResultLink[] = [];
-      for (const titleElement of resultElements) {
-        const title = (await titleElement.textContent()) || "";
-        const linkElement = await titleElement.$("a");
-        const linkUrl = await linkElement?.getAttribute("href");
+      const url = linkUrl ? await resolveBaiduLink(linkUrl) : "";
 
-        const url = linkUrl ? await resolveBaiduLink(linkUrl) : "";
+      results.push({ title, description: linkUrl!, url });
+    }
 
-        results.push({ title, description: linkUrl!, url });
-      }
+    const hotSearchResultElements = await page.$$("div[class^='toplist1-tr']");
+    const hotSearchResults: Link[] = [];
+    for (const hotSearchElement of hotSearchResultElements.slice(0, 16)) {
+      const title = (await hotSearchElement.textContent()) || "";
+      const linkElement = await hotSearchElement.$("a");
+      const url = (await linkElement?.getAttribute("href")) || "";
 
-      const hotSearchResultElements = await page.$$(
-        "div[class^='toplist1-tr']"
-      );
-      const hotSearchResults: Link[] = [];
-      for (const hotSearchElement of hotSearchResultElements.slice(0, 16)) {
-        const title = (await hotSearchElement.textContent()) || "";
-        const linkElement = await hotSearchElement.$("a");
-        const url = (await linkElement?.getAttribute("href")) || "";
+      hotSearchResults.push({ title, url });
+    }
 
-        hotSearchResults.push({ title, url });
-      }
+    const relatedKeywordsElements = await page.$$(
+      "div[tpl='recommend_list'] .c-gap-top-xsmall"
+    );
+    const relatedSearchKeywords: string[] = [];
+    for (const relatedKeywordsElement of relatedKeywordsElements) {
+      const title = (await relatedKeywordsElement.textContent()) || "";
+      relatedSearchKeywords.push(title);
+    }
 
-      const relatedKeywordsElements = await page.$$(
-        "div[tpl='recommend_list'] .c-gap-top-xsmall"
-      );
-      const relatedSearchKeywords: string[] = [];
-      for (const relatedKeywordsElement of relatedKeywordsElements) {
-        const title = (await relatedKeywordsElement.textContent()) || "";
-        relatedSearchKeywords.push(title);
-      }
+    const similarKeywordsElements = await page.$$("a[class^='rs-link']");
+    const similarSearchKeywords: string[] = [];
+    for (const similarKeywordsElement of similarKeywordsElements) {
+      const title = (await similarKeywordsElement.getAttribute("title")) || "";
+      similarSearchKeywords.push(title);
+    }
 
-      const similarKeywordsElements = await page.$$("a[class^='rs-link']");
-      const similarSearchKeywords: string[] = [];
-      for (const similarKeywordsElement of similarKeywordsElements) {
-        const title =
-          (await similarKeywordsElement.getAttribute("title")) || "";
-        similarSearchKeywords.push(title);
-      }
+    const resultsCountElement = await page.$("span[class^='hint']");
+    const resultsCount = resolveResultsCount(
+      (await resultsCountElement?.textContent()) || ""
+    );
 
-      const resultsCountElement = await page.$("span[class^='hint']");
-      const resultsCount = resolveResultsCount(
-        (await resultsCountElement?.textContent()) || ""
-      );
+    const dataStructure: DataResults = {
+      results,
+      hotSearchResults,
+      resultsCount,
+      similarSearchKeywords,
+      relatedSearchKeywords,
+    };
 
-      const dataStructure: DataResults = {
-        results,
-        hotSearchResults,
-        resultsCount,
-        similarSearchKeywords,
-        relatedSearchKeywords,
-      };
-
-      await dataset.pushData(dataStructure);
-    },
-  });
-
-  const { searchPhrases = ["apify"], pages = 1 } =
-    (await Actor.getInput<Input>()) ?? {};
-
-  const pageArray = [...Array(pages).keys()];
-
-  const requests = searchPhrases.flatMap((searchPhrase) =>
-    pageArray.map((pageNr) => {
-      const lookoutQuery = new URLSearchParams([
-        ["pn", pageNr.toString()],
-        ["wd", searchPhrase],
-      ]);
-      return `https://www.baidu.com/s?${lookoutQuery.toString()}`;
-    })
-  );
-
-  await crawler.addRequests(requests);
-  await crawler.run();
+    await Dataset.pushData(dataStructure);
+  },
 });
+
+const { searchPhrases = ["apify"], pages = 1 } =
+  (await KeyValueStore.getInput<Input>()) ?? {};
+
+const pageArray = [...Array(pages).keys()];
+
+const requests = searchPhrases.flatMap((searchPhrase) =>
+  pageArray.map((pageNr) => {
+    const lookoutQuery = new URLSearchParams([
+      ["pn", pageNr.toString()],
+      ["wd", searchPhrase],
+    ]);
+    return `https://www.baidu.com/s?${lookoutQuery.toString()}`;
+  })
+);
+
+await crawler.addRequests(requests);
+await crawler.run();
